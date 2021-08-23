cc6178a31b0ea91e6326783067f99135f6860bb4
Anup Samuel <57674855+anupsam7@users.noreply.github.com>
2021-08-23 09:26:33 -0400

    Add files via upload

---
+++ streamlit/Iris ML WebApp/iris_ml_app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Iris Flower Prediction WebApp
+
+This app predicts **Iris flower** types.
+""")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4) # shows the min, max and default values
+    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+    data = {'sepal_length':sepal_length,
+    'sepal_width':sepal_width,
+    'petal_length':petal_length,
+    'petal_width':petal_width
+    }
+
+    features = pd.DataFrame(data, index = [0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User input parameters')
+st.write(df)
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+clf = RandomForestClassifier()
+clf.fit(X,y)
+
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
+st.subheader('Class labels and their corresponding index number')
+st.write(iris.target_names)
+
+st.subheader('Prediction')
+st.write(iris.target_names[prediction])
+
+st.subheader('Prediction probability')
+st.write(prediction_proba)
